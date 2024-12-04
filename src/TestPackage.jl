a336b8d05895ea33851188e74df52145e83453bb
marckcode <marckcode@gmail.com>
2024-12-04 15:16:37 -0700

    add ForwardDiff dependency and add safe tests

--- src/TestPackage.jl
+++ src/TestPackage.jl
@@ -1,7 +1,10 @@
 module TestPackage
 
+using ForwardDiff
+
 include("extra_file.jl")
 
-export my_f 
+export my_f
+export derivative_of_my_f
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,3 @@
-using TestPackage
-using Test
+using SafeTestsets
 
-@testset "TestPackage.jl" begin
-    # 2x + 3y
-    @test my_f(2, 1) == 7
-    @test my_f(2, 3) == 13
-    @test my_f(1, 3) == 11
-end 
+@safetestset "My f tests" begin include("my_f_tests.jl") end
